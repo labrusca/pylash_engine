8dd2ecfbfb9f0825555539f4db33730f5bf45da6
labrusca <labrusca@live.cn>
2016-06-03 01:30:19 +0800

    add package files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+      name = "pylash" ,
+      version = "1.4.1" ,
+      description = "A game engine for python" ,
+      author = "labrusca" ,
+      url = "https://github.com/labrusca" ,
+      license = "MIT" ,
+      packages = find_packages(),
+)
+
+
